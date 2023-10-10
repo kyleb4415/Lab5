8b2c0e0ebb9109c70f4c62b962a6d94dd35dc5f0
KyleHelms4415 <113382650+kyleb4415@users.noreply.github.com>
2023-10-10 14:49:47 -0400

    Add files via upload

---
+++ Lab5MusicAPI/Lab5MusicAPI/Event.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Lab5MusicAPI
+{
+    public class Event
+    {
+        public string Id { get; set; }
+        public int Score { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        [JsonPropertyName("life-span")]
+        public Lifespan Lifespan { get; set; }
+        public Event(string id, int score, string name, string type, Lifespan lifespan)
+        {
+            this.Id = id;
+            this.Score = score;
+            this.Name = name;
+            this.Type = type;
+            this.Lifespan = lifespan;
+
+        }
+
+        public override string ToString()
+        {
+            string msg = $"Name: {Name}\n" +
+                $"Type: {Type}\n" +
+                $"Lifespan: \n{Lifespan}";
+            return msg;
+        }
+    }
+}
